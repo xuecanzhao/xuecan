570e57249560ef739b38b208f93c42a2477c92e1
xuecanzhao <461900137@qq.com>
2017-01-08 19:33:41 +0800

    Revert "V1.0"
    This reverts commit bbe3cc563e403cf9724730a57c5365557a4f13f7.

--- myGraduation/EdgeDetect.h
+++
@@ -1,6 +0,0 @@
-#ifndef IMGPREPROCESS_H
-#define IMGPREPROCESS_H
-#include<opencv2\opencv.hpp>
-using namespace cv;
-void edgeDetect(Mat &src,Mat& dst);
-#endif
--- myGraduation/Otsu.h
+++
@@ -1,6 +0,0 @@
-#ifndef OTSU_H
-#define OTSU_H
-#include<opencv2\opencv.hpp>
-using namespace cv;
-int Otsu(Mat& gray);
-#endif
